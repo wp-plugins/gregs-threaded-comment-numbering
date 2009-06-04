0d41be20195bbe2912bd09bef606384990e00e6b
GregMulhauser <GregMulhauser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-04 12:14:50 +0000

    1.2.2 update
    git-svn-id: https://plugins.svn.wordpress.org/gregs-threaded-comment-numbering/tags/1.2.2@122868 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gtcn-options-functions.php
@@ -0,0 +1,325 @@
+<?php
+
+/*  Copyright (c) 2009 Greg Mulhauser, http://counsellingresource.com
+
+	Developers: If you'd like to use this class for your own plugins, to avoid the monstrosity that is the usual method for creating WordPress options pages, please feel free. In return, I'd appreciate it if you could please mention my site and/or plugins on your own site, as well as keeping this message and copyright notice intact with any classes you distribute which are based on this class. (You know the drill: "lots of effort has gone into developing this software, blah blah blah".)
+	
+	The gregsOptionsHandler class is designed for use under WordPress 2.7+ and assumes that options have already been set up with add_option and register_setting. See the gregsSetupHandler class provided in the file ending "-setup-functions.php" for details on how options can easily be grouped into sets for use with this class. That same class also sets up the CSS which formats the options pages produced by this class.
+	
+	The class supports radio buttons, checkboxes, text, textarea, and select options, as well as options marked 'extra' which will just be displayed as-is, without any extra parsing or structure. (You might use the 'extra' type to display a donation button, for example.) It also supports a plain page of instructions for your plugin, which will just be loaded as-is, without any modifications.
+
+	This class assumes it sits within a file ending with "-functions.php", and it looks for ".ini" files named with the same pattern, minus the "-functions.php", and optionally located within a subdirectory that gets passed in. It has been done this way so if you only have one or two options pages and want to keep them at the same directory level as this class, you can do so, and everything will sort nicely -- but if you have a bunch of them, you can just dump them in a subdirectory.
+	
+	The options pages themselves are created out of ".ini" files parsed with a quick and dirty, PHP4-compatible reader that does only the bare minimum we need. The more capable PHP5 parse_ini_file might be more efficient -- I'm not sure -- but the PHP4 version of that function has problems which prevent it from doing the job for us, thus the quick and dirty function instead. If you would rather use the PHP5 version when running under PHP5, just un/comment the relevant lines in prep_settings.
+	
+	The rest will hopefully be self-explanatory if you have a peek at the options setup page which loads this class.
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+class gtcnOptionsHandler {
+
+var $replacements = array(); // set of replacements to perform during parsing
+var $pages = array(); // set of pages we're expecting, as an array with page name, page title, page menu entry
+var $domain; // our text translation domain
+var $plugin_prefix; // prefix for each option name
+var $oursettings; // holds full set of parsed settings
+var $page_title; // indicates main title for page, derived from array passed in
+var $instructions; // indicates whether we're handling an instructions page (i.e., no submit button)
+var $path; // where are we?
+var $thispage; // name of this page, from keys in var $pages
+
+function gtcnOptionsHandler($swap = array(),$pages = array(),$domain,$plugin_prefix='',$subdir='',$instname='') {
+$this->__construct($swap,$pages,$domain,$plugin_prefix,$subdir,$instname);
+return;
+} 
+
+function __construct($swap = array(), $pages = array(),$domain,$plugin_prefix='',$subdir='',$instname='') {
+$this->replacements = $swap;
+$this->domain = $domain;
+$this->plugin_prefix = $plugin_prefix;
+$this->pages = $pages;
+$dir = str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); // get plugin folder name
+$base = str_replace("-functions.php","",basename( __FILE__)); // get this file's name without extension, assuming it ends with '-functions.php'
+$this->path = $dir . $base;
+($subdir != '') ? $subdir .= '/' : $subdir = '';
+$this->oursettings = $this->prep_settings($dir . $subdir . $base,$instname);
+if (!($this->instructions)) $this->oursettings = array_map(array($this,'do_option_replacements'),$this->oursettings);
+return;
+} // end constructor
+
+function prep_settings($root = '',$instname='instructions') {
+$ourpages = $this->pages;
+$sub = isset ($_GET['submenu']) ? $_GET['submenu'] : '';
+($sub != '') ? $filetail = "-$sub" : $filetail = '';
+($sub == $instname) ? $this->instructions = true : $this->instructions = false;
+$toload = WP_PLUGIN_DIR . '/' . $root . $filetail . '.ini';
+if (file_exists($toload)) {
+	$this->thispage = ($sub != '') ? $sub : 'default';
+	if (array_key_exists($sub,$ourpages))
+	   $this->page_title = wptexturize(__($ourpages[$sub][0],$this->domain));
+	   else ($sub == '') ? $this->page_title = wptexturize(__($ourpages['default'][0],$this->domain)) : $this->page_title = '';
+	if ($this->instructions) $settings = file_get_contents($toload);
+	elseif (PHP_VERSION >= 5)
+// If you want to use the PHP5 function when available, uncommment the following line and comment out the line after
+//	   $settings = parse_ini_file($toload);
+	   $settings = $this->parse_ini_file_php4($toload);
+	   else $settings = $this->parse_ini_file_php4($toload);
+	   } // end action if corresponding ini file exists
+	else $settings = array();
+return $settings;
+} // end prepping settings
+
+function parse_ini_file_php4 ($file) {
+// very quick and dirty replacement because PHP 4.4.7 fails to load arrays properly
+$file_handle = fopen($file, "rb");
+while (!feof($file_handle) ) {
+$line_of_text = trim(fgets($file_handle),"\r\n ");
+if (strstr($line_of_text,';')) {
+   $temp = explode(';',$line_of_text);
+   $line_of_text = $temp[0];
+   } // end handling comments
+$firstchar = substr($line_of_text,0,1);
+if (!(($line_of_text == '') || ($firstchar == '['))) { // ignore sections and blanks
+	$parts = explode('=', $line_of_text);
+	$parts[0] = trim($parts[0],'[] ');
+	$parts[1] = trim($parts[1],' "');
+	$output[$parts[0]][]=$parts[1];
+	} // end handling only non-sections
+}
+fclose($file_handle);
+return $output;
+}
+
+function adust_setting_name($setting='') {
+return $this->plugin_prefix . $setting;
+} // end adjusting setting name
+
+function do_option_replacements($content='') {
+$content = str_replace(array_keys($this->replacements),array_values($this->replacements),$content);
+return $content;
+}
+
+function do_save_button($buttontext='Save Changes') {
+$button = __($buttontext, $this->domain);
+if ($this->instructions) $save = '';
+else $save = <<<EOT
+<table class="form-table">
+<tr valign="top">
+<th scope="row"></th>
+<td><p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="{$button}" />
+</p>
+</td>
+</tr>
+</table>
+EOT;
+return $save;
+} // end creating save button
+
+function do_pagemenu() {
+$output = '';
+$ourpages = $this->pages;
+if (count($ourpages) > 1) {
+	$output = "<div class='" . $this->plugin_prefix . "menu'>\n<ul>\n";
+	foreach ($ourpages as $page=>$details) {
+	   $menutitle = wptexturize(__($details[1],$this->domain));
+	   $menutitle = str_replace(' ','&nbsp;',$menutitle);
+	   if ( $this->thispage == $page )
+			$output .= "<li><strong>{$menutitle}</strong> | </li>";
+	   else { // do a link
+			$submenu = ($page == 'default') ? "" : "&amp;submenu={$page}";
+			$output .= "<li><a href=\"options-general.php?page={$this->path}.php{$submenu}\">{$menutitle}</a> | </li>";
+			} // end doing an actual link
+	   } // end loop over pages
+	$output = substr($output,0,strlen($output)-8) . '</li>'; // snip off the last ' | ' inside the <li>
+	$output .= "</ul>\n</div>\n";
+	} // end check for array with just one page
+return $output;
+} // end creating page menu
+
+function conflict_check($problemapps=array(),$name='') {
+$domain = $this->domain;
+$warningclass = $this->plugin_prefix . 'warning';
+foreach ($problemapps as $problemapp) {
+   $test = (array_key_exists('class',$problemapp)) ? 'class' : 'function';
+   $testfx = $test . '_exists';
+   if ($testfx($problemapp[$test])) {
+	   $conflict = $problemapp['name'];
+	   $warning = $problemapp['warning'];
+	   } // end testing for problem apps
+   } // end loop over problem apps
+if ($conflict == '') $message = '';
+else {
+$warningprefix = __('Warning: Possible conflict with', $domain);
+$warningend = __('For best results, please disable the interfering plugin',$domain);
+$message = <<<EOT
+<div class="{$warningclass}">
+<p><strong><em>{$warningprefix} '{$conflict}'</em></strong></p>
+<p>{$warning} <em>{$name}</em>.</p>
+<p>{$warningend} '{$conflict}'</strong>.</p>
+</div>
+EOT;
+}
+return wptexturize($message);
+} // end conflict check
+
+function display_options($settings_prefix='',$problems=array(),$name='',$dofull=true,$donated=false) {
+$body = $this->do_options($dofull,false);
+$title = $this->page_title;
+$save = $this->do_save_button();
+$menu = $this->do_pagemenu();
+$thispage = $this->thispage;
+$domain = $this->domain;
+$plugin_prefix = $this->plugin_prefix;
+$thankspre = __("Thank you for having contributed directly to the development effort or linking to:",$domain);
+$thankspost = __("I appreciate your support!",$domain);
+$donation = ($donated) ? wptexturize("<p class='{$plugin_prefix}thanks'>{$thankspre} {$name}. {$thankspost}</p>") : "";
+$conflict = $this->conflict_check($problems,$name);
+
+$displaytop = <<<EOT
+<div class="wrap">
+<form method="post" action="options.php"> 
+EOT;
+$displaybot = <<<EOT
+<h2>{$title}</h2>
+{$menu}
+{$donation}
+{$conflict}
+{$body}
+{$save}
+</form>
+</div>
+EOT;
+echo $displaytop;
+if (!$this->instructions) settings_fields($settings_prefix . $thispage);
+screen_icon();
+echo $displaybot;
+return;
+} // end displaying options
+
+function do_options($full=true,$echo=true) {
+$settings = $this->oursettings;
+$domain = $this->domain;
+if (!is_array($settings)) return wptexturize(__($settings,$domain));
+$output = '';
+$elements = count($settings['setting']);
+$stepper = '0';
+
+while ($stepper < $elements) {
+
+$header = wptexturize(__($settings['header'][$stepper], $domain));
+$preface = wptexturize(__($settings['preface'][$stepper], $domain));
+
+if ($header != '')
+	$output .= "<h3>{$header}</h3>\n";
+if (($preface != '') && $full)
+	$output .= "<p>$preface</p>\n";
+if (($header != '') || ($preface != ''))
+	$output .= '<table class="form-table ' . $this->plugin_prefix . 'table">';
+
+$output .=  '<tr valign="top"><th scope="row">' . $settings['label'][$stepper] . "</th>\n<td>\n";
+
+$properties = explode(',', $settings['type'][$stepper]);
+
+if ($properties[0] == 'text') {
+$settings['setting'][$stepper] = $this->adust_setting_name($settings['setting'][$stepper]);
+// we use wp_specialchars_decode first in case this field has htmlspecialchars set as its callback filter with register_settings
+// have to use wp_specialchars_decode TWICE because WP is double-specialcharring it
+$echosetting = htmlspecialchars(stripslashes(wp_specialchars_decode(wp_specialchars_decode(get_option($settings['setting'][$stepper], ENT_QUOTES), ENT_QUOTES))));
+$echodescription = wptexturize(__($settings['description'][$stepper], $domain));
+$output .= <<<EOT
+<input type="text" size="{$properties[1]}" name="{$settings['setting'][$stepper]}" value="{$echosetting}" />\n<br />{$echodescription}
+EOT;
+} // end handling text
+
+elseif ($properties[0] == 'textarea') {
+$settings['setting'][$stepper] = $this->adust_setting_name($settings['setting'][$stepper]);
+// we use wp_specialchars_decode first in case this field has htmlspecialchars set as its callback filter with register_settings
+// have to use wp_specialchars_decode TWICE because WP is double-specialcharring it
+$echotext = htmlspecialchars(stripslashes(wp_specialchars_decode(wp_specialchars_decode(get_option($settings['setting'][$stepper], ENT_QUOTES), ENT_QUOTES))));
+$output .= <<<EOT
+\n<textarea cols="{$properties[1]}" rows="$properties[2]" name="{$settings['setting'][$stepper]}">{$echotext}</textarea>\n
+EOT;
+$description = wptexturize(__($settings['description'][$stepper], $domain));
+if ($description != '')
+	$output .=  "<br />$description";
+} // end handling textarea
+
+elseif (($properties[0] == 'checkbox') || ($properties[0] == 'radio')) {
+$nowcounter = 0;
+$nowsettings = explode(',',$settings['setting'][$stepper]);
+$nowvalues = explode(',',$settings['value'][$stepper]);
+$nowdescriptions = explode('|',$settings['description'][$stepper]);
+$output .= "<ul>\n";
+while ($nowcounter < $properties[1]) {
+$nowsettings[$nowcounter] = $this->adust_setting_name($nowsettings[$nowcounter]);
+($properties[0] == 'checkbox') ?
+			$testcheck = $nowcounter : $testcheck = 0; // if radio button, only look at setting 0 in following test, because there is only one, otherwise step through the settings
+(get_option($nowsettings[$testcheck]) == $nowvalues[$nowcounter]) ?
+			$checked = ' checked="checked"' : $checked = '';
+$echodescription = wptexturize(__($nowdescriptions[$nowcounter],$domain));
+if ($properties[0] == 'checkbox')
+	$output .= <<<EOT
+<li><label for="{$nowsettings[$nowcounter]}"><input name="{$nowsettings[$nowcounter]}" type="checkbox" id="{$nowsettings[$nowcounter]}" value="{$nowvalues[$nowcounter]}"{$checked} />&nbsp;{$echodescription}</label></li>\n
+EOT;
+else $output .= <<<EOT
+<li><input type="radio" name="{$nowsettings[0]}" value="{$nowvalues[$nowcounter]}"{$checked} />&nbsp;{$echodescription}</li>\n
+EOT;
+$nowcounter ++;
+} // end loop over number of boxes or buttons
+$output .= "</ul>\n";
+} // end handling checkbox or radio
+
+elseif ($properties[0] == 'select') {
+$nowcounter = 0;
+$nowvalues = explode(',',$settings['value'][$stepper]);
+$nowdescriptions = explode('|',$settings['description'][$stepper]);
+$settings['setting'][$stepper] = $this->adust_setting_name($settings['setting'][$stepper]);
+$output .= '<select name="' . $settings['setting'][$stepper] . '" size="1">';
+while ($nowcounter < $properties[1]) {
+(get_option($settings['setting'][$stepper]) == $nowvalues[$nowcounter]) ?
+			$selected = ' selected="selected"' : $selected = '';
+$output .=  <<<EOT
+<option value="{$nowvalues[$nowcounter]}"{$selected}>{$nowdescriptions[$nowcounter]}</option>\n
+EOT;
+$nowcounter ++;
+} // end loop over select values
+$output .= "</select>\n";
+} // end handling select
+
+elseif ($properties[0] == 'extra')
+		$output .= wptexturize(__($settings['description'][$stepper], $domain));
+
+$output .= "\n</td>\n</tr>\n";
+
+if (($stepper + 1 == $elements) || ($settings['header'][$stepper + 1] != '') || ($settings['preface'][$stepper + 1] != '')) {
+$output .= '</table>';
+}
+
+$stepper ++;
+} // end loop over headings
+
+if ($echo)
+	echo $output;
+else return $output;
+
+return;
+
+} // end function which outputs options
+
+} // end class definition
+
+?>
--- readme.txt
+++ readme.txt
@@ -3,22 +3,33 @@ Contributors: GregMulhauser
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2799661
 Tags: comments, count, numbering, threading, paging, paged comments, threaded comments, pingback, trackback, display, callback function, comments.php, greg mulhauser, comment number, comment counter, listing comments
 Requires at least: 2.7
-Tested up to: 2.7
-Stable tag: 1.0.3
+Tested up to: 2.8-beta2
+Stable tag: 1.2.2
 
 Numbers comments sequentially and hierarchically; handles comments which are threaded, paged and/or reversed. Coders can call the function directly.
 
 == Description ==
 
-The introduction of WordPress 2.7 brought with it significant new capabilities for threading and paging comments, but these same changes in WordPress mean that well established methods for numbering comments -- like including a basic incrementing counter within your template code -- no longer do the trick. Fortunately, taking advantage of modern comment handling features like paging and threading doesn't have to mean giving up comment numbering.
+This plugin numbers your comments sequentially and hierarchically, with full support for the new comment features introduced in WordPress 2.7 -- including threading, paging, and your choice of ascending or descending date order.
 
-Coupled with a new template function for displaying comments which debuted in WordPress 2.7, Greg's Threaded Comment Numbering plugin provides accurate sequential numbering for each comment, including hierarchical numbering for the first level of threaded comments (e.g., comment number 2 and its replies numbered 2.1, 2.2, 2.3, etc.).
+= New in This Version =
+
+* Improved documentation
+* Support for WordPress 2.8
+
+For more details on what's new in the latest main release, version 1.2, please see the update announcement: [WordPress Threaded Comment Numbering Plugin Updated](http://counsellingresource.com/features/2009/03/31/threaded-comment-plugin-updated)
+
+= Background =
+
+The introduction of WordPress 2.7 brought with it significant new capabilities for threading and paging comments, but these same changes in WordPress mean that well established methods for numbering comments -- like including a basic incrementing counter within your template code -- no longer do the trick. Fortunately, taking advantage of modern comment handling features like paging and threading doesn't have to mean giving up comment numbering altogether.
+
+Coupled with a new template function for displaying comments which debuted in WordPress 2.7, Greg's Threaded Comment Numbering plugin provides accurate sequential numbering for each comment, including hierarchical numbering up to the full 10 levels supported by WordPress.
 
 The plugin numbers comments accurately whether you choose to display them in ascending or descending date order, on multiple pages or on one long page, and with or without threading enabled. It also handles pingback and trackback numbering.
 
 For coders, the plugin provides additional configuration options via direct calls to the function that handles the numbering.
 
-For more information, please see this plugin's information page: [Greg's Threaded Comment Numbering Plugin](http://counsellingresource.com/features/2009/01/27/threaded-comment-numbering-plugin-for-wordpress/)
+For more information, please see this plugin's original information page: [Greg's Threaded Comment Numbering Plugin](http://counsellingresource.com/features/2009/01/27/threaded-comment-numbering-plugin-for-wordpress/)
 
 == Installation ==
 
@@ -34,9 +45,11 @@ With a single line of code -- either within `comments.php` or within a callback 
 
 *Basic Usage*
 
-If you are using the default theme, or another theme which already supports the new WordPress 2.7 comments features but does not use its own callback function, just replace `wp_list_comments()` in your theme's `comments.php` file with `wp_list_comments('callback=gtcn_basic_callback')`. (See below for a note on safe wrapping of plugin-dependent function calls.)
+If you are using the default theme, or another theme which already supports the new WordPress 2.7 comments features but does not use its own callback function, you may replace `wp_list_comments()` in your theme's `comments.php` file with `wp_list_comments('callback=gtcn_basic_callback')`. (See below for a note on safe wrapping of plugin-dependent function calls.)
 
-The plugin includes some basic styling information suitable for the default theme, but if you'd rather style numbers yourself, you can disable this feature in the plugin's settings page and provide your own within your template's CSS file.
+Please note, however, that relying on the basic included callback function means that any existing structure or styling you have put in place for your comments will be replaced by what is in the callback function. It is preferable to create your own callback function, drop it into `functions.php`, and use that to ensure your comments are structured and styled exactly the way you'd like them to be -- see 'Advanced Usage' below for more on this option.
+
+The plugin does include some basic styling information suitable for the default theme, but if you'd rather style numbers yourself, you can disable this feature in the plugin's settings page and provide your own within your template's CSS file.
 
 *Advanced Usage*
 
@@ -75,7 +88,39 @@ I've moved this section of the README to the main information page for the plugi
 1. Basic threaded comment numbering configuration options
 2. Hierarchical comment numbering using the default theme and the provided styling
 
+== Known Issues ==
+
+The version of the Intense Debate plugin which is current as of this writing overrides themes' existing comment code, rendering it incompatible with any theme or plugin designed to use the full capabilities of the new `wp_list_comments` introduced in WordPress 2.7. This means that regardless of what changes you make to your theme's comment code (such as calling this plugin), those changes won't show up while your comments are being controlled by Intense Debate.
+
 == Revision History ==
+
+**1.2.2, 4 June 2009**
+
+* Updated documentation
+* Support for WordPress 2.8
+
+**1.2.1, 15 April 2009**
+
+* Fixed a minor typo which would have interfered with translations for this plugin -- thanks to Nikolay
+
+**1.2, 31 March 2009**
+
+* This version brings higher performance, several minor enhancements, and a revamped administrative interface; it is recommended for all users.
+
+**1.1.2, 13 February 2009**
+
+* In return for a slight performance hit, Greg's Threaded Comment Numbering plugin can now check explicitly whether a threaded comment has been orphaned by having its parent comment deleted -- in which case, WordPress may display the comment in the wrong order. Due to the slight decrease in performance, this feature should only be enabled if you are experiencing problems with orphaned comment ordering.
+
+**1.1.1, 3 February 2009**
+
+* Folks who didn't notice the README note to update their preferred level of hierarchical numbering will find that the code now does it for them
+
+**1.1, 2 February 2009**
+
+* New feature: increased hierarchical numbering from 2 levels to 10 levels
+* New feature: 'jumble count' mode for time-ordered numbering
+* Enhancement: rewritten core numbering routines for significantly improved efficiency
+* Fixed: cleaned up 'path to url' text (left over from WordPress Codex) in the provided basic callback function, restoring default avatar features for users calling the basic function
 
 **1.0.3, 30 January 2009**
 
