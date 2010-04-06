a46a2a789d42bbdae6878ba9e9c6ecc198c5110e
GregMulhauser <GregMulhauser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-06 15:12:29 +0000

    1.3.3 update
    git-svn-id: https://plugins.svn.wordpress.org/gregs-threaded-comment-numbering/tags/1.3.3@225770 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gtcn-setup-functions.php
+++ gtcn-setup-functions.php
@@ -1,5 +1,27 @@
 <?php
 
+// This setup class is only loaded if we're actually on admin pages
+
+/*
+<rant>
+
+	We now bring you a special note especially for the self-appointed thought police of WordPress plugin programming who would like everyone to believe it's just plain obvious that good plugins always store their options in a single array or object, while any plugin that adds multiple rows to the WordPress options table must have been written by an idiot...
+	
+	A couple of years ago, Stephen Rider wrote an excellent article about the messiness that can be created when plugin authors haphazardly add lots of rows to the WordPress options table. That article (http://striderweb.com/nerdaphernalia/2008/07/consolidate-options-with-arrays/) was GREAT, but unfortunately several subsequent authors with seemingly diminished capacity for original thought have parroted the *conclusion* (namely, that a particular example plugin would have been 'better' with a single serialized array of options) and extrapolated it into a one-size-fits-all judgement about THE RIGHT WAY OF DOING THINGS -- without ever engaging in supporting reasoning or empirical data collection. Some now routinely disparage any plugin that does not do things THEIR way, apparently without realizing how idiotic the inference is from the case of one inefficiently coded plugin to the space of all possible plugins. (This is akin to another condescension promulgated by a hyper-judgemental minority of the thought police: namely, that if a plugin doesn't employ their preferred approach to whitespace and indentation, then it must be 'bad code'.)
+	
+	Folks who do their own thinking about these things discover that actually, it's not at all trivial to understand how lookup time scales as more and more options are added to a serialised array (or object) or how writing time is impacted when storing an entire serialised array just to update one single option within it. It's simply screwy to ASSUME that decreasing options table rows necessarily garners ANY increase in overall efficiency, given all the extra overheads associated with reading a big chunk of serialised options just to get at some sub-set of those options or writing a big chunk just to change one or two settings. And do the thought police have any idea how much harder it is to SEARCH a database for serialised array contents? (Don't get me started on the even more spurious argument that plugins ought to store one options array in order to make it easier for blog owners to manually comb through their options tables and delete them. Ever heard of the standard WordPress uninstall hook and delete_option?)
+	
+	Personally, I make a lot of mistakes in my programming, I know I miss 'obvious' efficiency improvements, and in a lot of cases the manifestly 'better' way of doing things totally eludes me. But this is not one of those cases.
+	
+	To the thought police: if it's such an obviously good idea to mash large numbers of options into one gigantic array that must always be read and written in its entirety, why not just step up to the plate and contribute code to the WordPress core so it ALWAYS stores a single plugin's options in a single row, and stop wasting everyone's time ranting about plugin authors who don't do it your way? If register_settings can handle a whole set of plugin options in a coherent way, surely you can think of an analogous approach for abstracting away from individual option storage and retrieval? But oh, wait -- it is NOT so obvious that this is always a good idea, and maybe if it really were so obvious, then WordPress would already have been designed that way in the first place.
+	
+	The bottom line, IMHO... If a plugin is only using a small amount of options storage anyway, chill out: whether it uses 1 row in the options table or 5 is not a big deal either way, and surely there are more important things in life to worry about. As the number of options rows increases, though, don't presuppose to know anything at all about what is more efficient or ultimately 'better' for the database or the blog or the blog owner. If it really keeps you awake at night, how about gathering some empirical data and sharing some conclusions based on reality? Then more folks will sit up and take you more seriously. I'll certainly be among the first to revise my options handling code if anybody does come up with some real and relevant data on the topic, as distinct from mere dogmatic dumpings of derision on plugin authors who don't subscribe to the thought police code.
+	
+	Until then, blog owners and plugin authors have better things to do with their time.
+
+</rant>
+*/
+
 class gtcnSetupHandler {
 
 var $plugin_prefix; // prefix for this plugin
@@ -13,17 +35,20 @@ return;
 function __construct($plugin_prefix,$location_full,$location_local,$options_page_details) {
 $this->plugin_prefix = $plugin_prefix;
 $this->options_page_details = $options_page_details;
+   // set up all our admin necessities
    add_filter( "plugin_action_links_{$location_local}", array(&$this,'plugin_settings_link'));
    add_action('admin_menu', array(&$this,'plugin_menu'));
+   add_action('admin_menu', array(&$this,'wp_postbox_js'));
    add_action('admin_init', array(&$this,'admin_init') );
    add_action('admin_head', array(&$this,'styles') );
    register_activation_hook($location_full, array(&$this,'activate') );
-
 return;
 } // end constructor
 
-function grab_settings() {
-// array keys correspond to the page of options on which the option gets handled
+function grab_settings() { // simple holder for all our plugin's settings
+
+// array keys correspond to the page of options on which that option gets handled
+// option array itself holds option name, default value, sanitization function
 
 $options_set = array(
 'default' => array(
@@ -45,7 +70,7 @@ $options_set = array(
 return $options_set;
 } // end settings grabber
 
-function activate() {
+function activate() { // on activation, set up our options
 $options_set = $this->grab_settings();
 $prefix = $this->plugin_prefix . '_';
 foreach ($options_set as $optionset=>$optionarray) {
@@ -56,7 +81,7 @@ foreach ($options_set as $optionset=>$optionarray) {
 return;
 }
 
-function admin_init(){
+function admin_init(){ // register our settings
 $options_set = $this->grab_settings();
 $prefix_setting = $this->plugin_prefix . '_options_';
 $prefix = $this->plugin_prefix . '_';
@@ -70,11 +95,29 @@ return;
 
 function plugin_menu() {
 $details = $this->options_page_details;
-  add_options_page("{$details[0]}", "{$details[1]}", 'manage_options', "{$details[2]}");
+$page_hook = add_options_page("{$details[0]}", "{$details[1]}", 'manage_options', "{$details[2]}");
+// NOTE: WP's system for unobtrusively inserting JS, css, etc. only on pages that are needed, documented in several places such as at http://codex.wordpress.org/Function_Reference/wp_enqueue_script appears to be broken when we're using another separate options page, so we'll have to do it the clunky way, with a URL check in the delivering function instead, and putting the add_action up in the constructor
+//add_action('admin_print_scripts-' . $page_hook, array(&$this,'wp_postbox_js'));
 return;
 }
 
-function plugin_settings_link($links) {
+function pay_attention() {
+// See note on plugin_menu function as to why we're doing this the crazy clunky way
+$page = $this->options_page_details[2];
+if (strpos(urldecode($_SERVER['REQUEST_URI']), $page) === false) return false;
+else return true;
+}
+
+function wp_postbox_js() {
+// See note on plugin_menu function as to why we're doing this check the crazy clunky way
+if (!$this->pay_attention()) return;
+wp_enqueue_script('common');
+wp_enqueue_script('wp-lists');
+wp_enqueue_script('postbox');
+return;
+}
+
+function plugin_settings_link($links) { // add our settings link to entry in plugin list
 $prefix = $this->plugin_prefix;
 $here = str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); // get plugin folder name
 $settings = "options-general.php?page={$here}{$prefix}-options.php";
@@ -83,23 +126,27 @@ array_unshift( $links, $settings_link );
 return $links;
 } // end settings link
 
-function styles() {
+function styles() { // we'll need a few styles for our options pages
+// See note on plugin_menu function as to why we're doing this check the crazy clunky way
+if (!$this->pay_attention()) return;
 $prefix = $this->plugin_prefix . '_';
 echo <<<EOT
 <style type="text/css">
-.{$prefix}table ul {padding-top:.5em;}
-.{$prefix}table th {text-align:right;}
+#poststuff .inside p {font-size:1.1em;}
+.{$prefix}table th {text-align:right; font-weight:bold; color:#333;}
 .{$prefix}menu ul, .{$prefix}menu li {display:inline;line-height:1.8em;}
 .{$prefix}menu {margin:15px 0;}
 .{$prefix}menu li a {text-decoration:none;}
-.{$prefix}thanks {font-style:italic;font-weight:bold;color:purple;}
+.{$prefix}thanks {font-style:italic;font-weight:bold;color:purple;padding:1.5em;border:1px dotted grey;}
 .{$prefix}warning {margin:2.5em;padding:1.5em;border:1px solid red;background-color:white;}
-.{$prefix}aside {float:right;margin:0 0 1em 1em;padding:.5em 1em;border:1px solid grey;width:300px;background-color:white;}
-.{$prefix}aside h4 {margin-top:0;padding-top:.5em;}
+.{$prefix}aside, .{$prefix}toc {float:right;margin:0 0 1em 1em;padding:.5em 1em;border:1px solid grey;width:300px;background-color:white;}
+.{$prefix}toc {float:left;margin:0 1em 1em 0;width:200px;}
+.{$prefix}toc ul ul {margin:.5em 0 0 1em;}
+.{$prefix}aside h4, .{$prefix}toc h4 {margin-top:0;padding-top:.5em;}
+ol.{$prefix}numlist {list-style-type:decimal;padding-left:2em;margin-left:0;}
 .{$prefix}fine_print {font-size:.8em;font-style:italic;}
 </style>
 EOT;
-return;
 return;
 } // end admin styles
 
